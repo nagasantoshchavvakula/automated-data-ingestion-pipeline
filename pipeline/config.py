049ec05bfd120ce9e08b732bf085a781e4e479b7
Chandrashekar <nagasantoshchavvakula@gmail.com>
2026-08-09 13:32:17 -0400

    feat: Updated config with base url

--- pipeline/config.py
+++ pipeline/config.py
@@ -9,7 +9,9 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
+API_BASE_URL = os.getenv("API_BASE_URL")
+
 DATABASE_URL = (
     f"postgresql://{DB_USER}:{DB_PASS}"
     f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
+)   
